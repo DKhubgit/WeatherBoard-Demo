9a58466e1f31f663e858b536327fb56b6e99713e
DanielK <Danielkang13@gmail.com>
2022-07-09 19:01:24 -0700

    updated fetch operations, added new functions, displays location on weather box

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,15 @@
 
 
+
 function getGeoLocation() {
-    var userCity = document.querySelector('input').value;
     var cityLat;
     var cityLon; 
-
+    var City = document.querySelector("input").value;
     //API url to get city's geolocation
-    var urlGeoLoc = "https://api.openweathermap.org/geo/1.0/direct?q=" + userCity + "&limit=1&appid=a10da6f6b4acf36309c658a9bd76edff";
+    var urlGeoLoc = "https://api.openweathermap.org/geo/1.0/direct?q=" + City + "&limit=1&appid=a10da6f6b4acf36309c658a9bd76edff";
 
     //calls the geolocation API from openweathermap
-    fetch(urlGeoLoc)
+    return fetch(urlGeoLoc)
      .then(response => {
         if (response.ok) {
             return response.json();
@@ -20,6 +20,10 @@ function getGeoLocation() {
      .then(data => { //data is an array of objects and need to access index 0 for lat and lon
         cityLat = data[0].lat;
         cityLon = data[0].lon;
+        var Name = data[0].name;
+        var State = data[0].state;
+        var Country = data[0].country;
+        displayLocation(Name, State, Country);
         getCurrWeather(cityLat, cityLon);
      })
      .catch(error => {
@@ -28,14 +32,11 @@ function getGeoLocation() {
 
          //API url for the future days forecast weather
          var urlFutureWeather = "https://api.openweathermap.org/data/2.5/forecast?lat=" + cityLat + "&lon=" + cityLon + "&appid=a10da6f6b4acf36309c658a9bd76edff";
-    
-    //clears the input field
-    document.querySelector('input').value = "";
 }
 
 //gets the Current weather
 function getCurrWeather(Lat,Lon) {
-    var urlCurrWeather = "https://api.openweathermap.org/data/2.5/weather?lat=" + Lat + "&lon=" + Lon + "&appid=4ab78489d29f98fdcfcf8fe40a55555c";
+    var urlCurrWeather = "https://api.openweathermap.org/data/2.5/onecall?lat=" + Lat + "&lon=" + Lon + "&exclude=minutely,hourly,daily,alerts&units=imperial&appid=4ab78489d29f98fdcfcf8fe40a55555c";
     //calls the current weather API, checks response and catch
     fetch(urlCurrWeather)
      .then(response => {
@@ -47,10 +48,41 @@ function getCurrWeather(Lat,Lon) {
     })
      .then(data => {
         console.log(data);
+        displayWeather(data);
     })
      .catch(error => {
         console.error('There was an error with 2nd Fetch operation', error);
     })
+}
+
+function displayLocation(Name, State, Country) {
+    var weatherBox = document.querySelector('.today-weather');
+    if (weatherBox.children.length === 0) { //if there is nothing in the box
+        var title = document.createElement('h1');
+        title.textContent = Name + ', ' + State + ' ' + Country;
+        document.querySelector('.today-weather').append(title);
+    } else { 
+        weatherBox.firstChild.remove();
+        var title = document.createElement('h1');
+        title.textContent = Name + ', ' + State + ' ' + Country;
+        document.querySelector('.today-weather').append(title);
+    }
+}
+
+//city name, the date, an icon representation of weather conditions,
+//the temperature, the humidity, the wind speed, and the UV index
+function displayWeather(data) {
+    var city = data.name
+    var date = new Date(data.current.dt * 1000); //date is in milliseconds so multiple 1000
+    var icon = data.current.weather[0].icon;
+    var temp = data.current.temp; //Fahrenheit
+    var humid = data.current.humidity; //in percentage %
+    var wind = data.current.wind_speed; //metre/sec
+    var UV = data.current.uvi;
+
+
+    //clears the input field
+    document.querySelector('input').value = "";
 }
 
 //waits for the 'find weather' button to be clicked
